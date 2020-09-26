adf15975a314e360cb6fc0f2d037497cf5c34937
ALinuxPerson <micheal02052007@gmail.com>
2020-09-26 21:39:00 +0800

    Implement basic 'dummy' source

---
+++ single/sources/dummy/__init__.py
@@ -0,0 +1,31 @@
+from single import Source, Flags, Package
+import typing as t
+
+
+class DummySource(Source):
+    @property
+    def FLAGS(self) -> t.List[Flags]:
+        return [
+            Flags.ALL_OS_SUPPORTED,
+            Flags.DOWNGRADE_SUPPORTED,
+            Flags.PARTIAL_UPGRADES_SUPPORTED,
+        ]
+
+    @property
+    def version(self) -> str:
+        return "0.1.0"
+
+    def supported(self) -> bool:
+        return True
+
+    def package(self, name: str) -> t.List[Package]:
+        return [Package("dummy", "0.1.0", "This is a dummy package.", 2.0, 1.0)]
+
+    def install_package(self, package: Package) -> None:
+        pass
+
+    def remove_package(self, package: Package) -> None:
+        pass
+
+    def update_package(self, package: Package) -> None:
+        pass
