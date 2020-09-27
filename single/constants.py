159dbedce8c19c7753519e0defd41fbe3813db48
ALinuxPerson <micheal02052007@gmail.com>
2020-09-27 08:57:33 +0800

    Implement all source directories in a list and fix 'str object is not callable' error in USER_DATA_DIR

--- single/constants.py
+++ single/constants.py
@@ -2,10 +2,12 @@
 import appdirs  # type: ignore
 from single import __version__ as version
 from pathlib import Path
+import typing as t
 
 APP_NAME: str = "single"
 AUTHOR: str = "ALinuxPerson"
 _appdirs: appdirs.AppDirs = appdirs.AppDirs(APP_NAME, AUTHOR, version)
-USER_DATA_DIR: Path = Path(_appdirs.user_data_dir())
+USER_DATA_DIR: Path = Path(_appdirs.user_data_dir)
 USER_SOURCES_DIR: Path = USER_DATA_DIR / "sources"
 GLOBAL_SOURCES_DIR: Path = Path(__file__) / "sources"
+SOURCES_DIRS: t.List[Path] = [GLOBAL_SOURCES_DIR, USER_SOURCES_DIR]
