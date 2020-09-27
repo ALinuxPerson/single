b0b00e7337cf7713342d408d28abe8e08fef5bb5
ALinuxPerson <micheal02052007@gmail.com>
2020-09-27 09:22:07 +0800

    Remove version from appdirs and fix global sources dir

--- single/constants.py
+++ single/constants.py
@@ -1,13 +1,12 @@
 """These contain some common constants for single."""
 import appdirs  # type: ignore
-from single import __version__ as version
 from pathlib import Path
 import typing as t
 
 APP_NAME: str = "single"
 AUTHOR: str = "ALinuxPerson"
-_appdirs: appdirs.AppDirs = appdirs.AppDirs(APP_NAME, AUTHOR, version)
+_appdirs: appdirs.AppDirs = appdirs.AppDirs(APP_NAME, AUTHOR)
 USER_DATA_DIR: Path = Path(_appdirs.user_data_dir)
 USER_SOURCES_DIR: Path = USER_DATA_DIR / "sources"
-GLOBAL_SOURCES_DIR: Path = Path(__file__) / "sources"
+GLOBAL_SOURCES_DIR: Path = Path(__file__).parent / "sources"
 SOURCES_DIRS: t.List[Path] = [GLOBAL_SOURCES_DIR, USER_SOURCES_DIR]
