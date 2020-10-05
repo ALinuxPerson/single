e5be4885027ba31701aacda6ff0bf3f340b46a4e
ALinuxPerson <alinuxperson@gmail.com>
2020-10-05 16:13:33 +0800

    Add docstrings to exceptions.py

--- single/exceptions.py
+++ single/exceptions.py
@@ -1,8 +1,23 @@
+"""These are some exceptions that should be raised by sources."""
+
+
 class UnsupportedSystemError(Exception):
+    """This is an error that should be raised on Source.supported if checks fail."""
+
     @property
     def message(self) -> str:
+        """The message.
+
+        Returns:
+            The message.
+        """
         return self.args[0]
 
     @property
     def action_needed(self) -> str:
+        """The actions needed for a source to work.
+
+        Returns:
+            The actions needed for a source to work.
+        """
         return self.args[1]
