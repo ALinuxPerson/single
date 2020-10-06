e87d7ebab2f51a56c8324630fede63fc25adbd1b
ALinuxPerson <alinuxperson@gmail.com>
2020-10-06 09:23:47 +0800

    Port SingleThreadedServer and partial parts of SinglePackageManagerService to new server

--- single/server/core.py
+++ single/server/core.py
@@ -2,6 +2,8 @@ from single.server import enums
 from single.server import utils
 from single.core import ProviderMetadata
 from loguru import logger
+from rpyc.utils.server import ThreadedServer
+from rpyc import Service
 import typing as t
 
 providers: t.List[ProviderMetadata] = []
@@ -14,4 +16,46 @@ def prepare_server(logging_level: enums.LoggingLevel) -> None:
 
 
 def start(port: int, logging_level: enums.LoggingLevel) -> None:
-    pass
+    prepare_server(logging_level)
+    try:
+        server = SingleThreadedServer(SinglePackageManagerService, port=port)
+        server.start()
+    except OverflowError:
+        logger.critical(f"The port {port} is not within 0-65535.")
+
+
+class SingleThreadedServer(ThreadedServer):
+    def _serve_client(self, sock, credentials):
+        client_host, client_port = sock.getpeername()
+        if credentials:
+            logger.info(
+                f"A client ({client_host}) has connected to the server using the port {client_port} "
+                f"(creds is {credentials})"
+            )
+        else:
+            logger.info(
+                f"A client ({client_host}) has connected to the server using the port {client_port}"
+            )
+        super()._serve_client(sock, credentials)
+
+    def start(self):
+        logger.debug("Listening...")
+        self._listen()
+        logger.debug("Registering...")
+        self._register()
+        logger.info(f"Server is now active and is listening on port {self.port}")
+        try:
+            while self.active:
+                self.accept()
+        except EOFError:
+            logger.info("Server closed by another thread")
+        except KeyboardInterrupt:
+            logger.info("A keyboard interrupt has been received, stopping server")
+        finally:
+            logger.info("The server has been terminated")
+            self.close()
+
+
+class SinglePackageManagerService(Service):
+    def exposed_status(self) -> None:
+        pass
