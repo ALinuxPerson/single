060755ba006c0ef4c85aedf15d19204293606a0a
ALinuxPerson <alinuxperson@gmail.com>
2020-10-12 16:26:05 +0800

    Add more docstrings to _enums.py

--- single/_enums.py
+++ single/_enums.py
@@ -3,6 +3,8 @@ import enum as e
 
 
 class System(e.Enum):
+    """This is a less error prone way of getting the system os name."""
+
     WINDOWS = "Windows"
     MAC = "Darwin"
     LINUX = "Linux"
@@ -10,6 +12,8 @@ class System(e.Enum):
 
 
 class LoggingLevel(e.Enum):
+    """This is a less error prone way of getting the logging level."""
+
     TRACE = "TRACE"
     DEBUG = "DEBUG"
     INFO = "INFO"
