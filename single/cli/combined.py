e779952391d829c8d0a4c9bca97ae4b02187d23b
ALinuxPerson <alinuxperson@gmail.com>
2020-10-16 11:55:20 +0800

    Add better help for combined command line

--- single/cli/combined.py
+++ single/cli/combined.py
@@ -2,7 +2,10 @@ from single.cli.client import app as client
 from single.cli.server import app as server
 import typer as ty
 
-app = ty.Typer()
+app = ty.Typer(
+    help="This is the combined command line for the single client and the single server. If you want to "
+    "use them individually, use 'singlec' for the client and 'singles' for the server."
+)
 app.add_typer(client, name="client")
 app.add_typer(server, name="server")
 
