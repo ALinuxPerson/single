02a6d237484844ec89879ac71cc57ff42982aa1a
ALinuxPerson <alinuxperson@gmail.com>
2020-10-17 19:56:57 +0800

    Add System import

--- single/__init__.py
+++ single/__init__.py
@@ -1,5 +1,6 @@
 """These contain the most common imports a developer would have and the version number for single."""
 from single.models import Package, Source
 from single.exceptions import UnsupportedSystemError
+from single.enums import System
 
 __version__ = "0.1.0"
